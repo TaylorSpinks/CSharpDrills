2db5af4e59f4bf7882e5cb2ec8b12f6d51398d79
taylorspinks <taylor@taylorspinks.tech>
2025-05-13 13:01:45 +1000

    Add drill files and first solution

---
+++ CSharpDrills/DrillTests.cs
@@ -0,0 +1,44 @@
+using Xunit;
+
+namespace CSharpDrills;
+
+public class DrillTests
+{
+    [Fact]
+    public void TestRemoveDuplicates()
+    {
+        int[] nums = { 1, 3, 2, 1, 3, 5 };
+
+        int[] nonDuplicatedNums = Drills.RemoveDuplicatesAndPreserveOrder(nums);
+        
+        int[] expected = { 1, 3, 2, 5 };
+        
+        Assert.Equal(expected, nonDuplicatedNums);
+    }
+
+    [Fact]
+    public void TestRemoveDuplicatesNextOrder()
+    {
+        int[] nums = { 4, 2, 2, 4, 3, 1 };
+
+        int[] nonDuplicatedNums = Drills.RemoveDuplicatesAndPreserveOrder(nums);
+
+        int[] expected = { 4, 2, 3, 1 };
+        
+        Assert.Equal(expected, nonDuplicatedNums);
+    }
+    
+    [Fact]
+    public void TestRemoveDuplicatesOrderBreak()
+    {
+        int[] nums = Enumerable.Range(0, 1000)
+            .Select(i => i % 100)
+            .ToArray();
+
+        int[] expected = Enumerable.Range(0, 100).ToArray();
+
+        var result = Drills.RemoveDuplicatesAndPreserveOrder(nums);
+    
+        Assert.Equal(expected, result);
+    }
+}
---
+++ CSharpDrills/Drills.cs
@@ -0,0 +1,30 @@
+namespace CSharpDrills;
+
+public class Drills
+{   
+    // not guaranteed, although I couldn't break in my tests
+    public static int[] RemoveDuplicatesAndPreserveOrder(int[] arrayWithDuplicates)
+    {
+        HashSet<int> duplicates = new HashSet<int>(arrayWithDuplicates);
+        return duplicates.ToArray();
+    }
+    
+    // better
+    public static int[] RemoveDuplicatesAndPreserveOrderV2(int[] arrayWithDuplicates)
+    {
+        var seen = new HashSet<int>();
+        var result = new List<int>();
+
+        foreach (int num in arrayWithDuplicates)
+        {
+            if (seen.Add(num))
+            {
+                result.Add(num);
+            }
+        }
+
+        return result.ToArray();
+    }
+    
+    
+}
